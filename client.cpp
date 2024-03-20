08be96f12e2763cd8089c4bf2cc610a05c87aa2d
puddosky <simonepuddu271205@gmail.com>
2024-03-20 08:40:12 +0100

    versione 1.0

--- client.cpp
+++ client.cpp
@@ -1,46 +1,6 @@
-//#ifdef __linux__
-#include <sys/socket.h>
-#include <stdio.h>
+#include <iostream>
 #include <cstring>
 
-void menu(){
-    std::cout<<"Digit a number and press ENTER to choose the option"<<std::endl;
-    std::cout<<"1. Show all the Votes"<<std::endl;
-    std::cout<<"2. Add a new Vote"<<std::endl;
-    std::cout<<"3. Show all the Votes of a student"<<std::endl;
-    std::cout<<"0. Exit the Program"<<std::endl;
-    std::cout<<std::endl;
-    return;
-}
-
-bool valuateOptions(){
-    String q;
-    std::cin >> q;
-    try {
-        int i = std::stoi(q);
-    } catch (std::exception const & e) {
-        std::cout<< 
-    }
-}
-
-void serverCommunication(int clientSocket){
-    bool end = false;
-    while(!end){
-        menu();
-        end = valuateOptions();
-    }
-    return;
-}
-
 int main(){
-    int clientSocket = socket(AF_INTE,SOCK_STREAM,0);
-    sockaddr_in serverAddress;
-    serverAddress.sin_family = AF_INET;
-    serverAddress.sin_port = htons(4721);
-    serverAddress.sin_addr.s_addr = INADDR_ANY;
-    connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
-    serverCommunication(clientSocket);
-    close(clientSocket);
-    return 1;
+    std::cout<<"ciao"<<std::endl<<"Ultima versione: 1.0";
 }
-//#endif
